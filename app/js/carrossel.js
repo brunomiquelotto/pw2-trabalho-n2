40e703e802ec53eeac0d7255d6dce677e0b7d14a
Bruno <bruno.miquelotto.bm@gmail.com>
2017-05-19 13:45:59 -0300

    Organização do projeto Imagens nos cards Empresa.html

---
+++ app/js/carrossel.js
@@ -0,0 +1,21 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoHeight: false,
+    autoHeightClass: 'owl-height',
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
